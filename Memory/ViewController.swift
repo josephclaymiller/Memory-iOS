efad6428ae39ba731a52f7e265bfb08f1a0571d5
Joseph Miller <josephclaymiller@gmail.com>
2018-03-22 13:57:56 -0700

    card borders

--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var cardBackColor: UIColor = UIColor.black
     var cardFrontColor: UIColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+    var cardBorderColor: UIColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     lazy var game = Memory(numberOfPairsOfCards: (cardButtons.count + 1)/2)
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var flipCountLabel: UILabel!
@@ -38,31 +39,37 @@ class ViewController: UIViewController {
             gameBoard.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
             cardBackColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
             cardFrontColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+            cardBorderColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
         case .animalHeads:
             emojiChoices = animalHeads
             gameBoard.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
             cardBackColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
             cardFrontColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            cardBorderColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         case .catHeads:
             emojiChoices = catHeads
             gameBoard.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
             cardBackColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
             cardFrontColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            cardBorderColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
         case .plants:
             emojiChoices = plantEmojis
             gameBoard.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
             cardBackColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
             cardFrontColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+            cardBorderColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
         case .food:
             emojiChoices = foodEmojis
             gameBoard.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
             cardBackColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
             cardFrontColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            cardBorderColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
         default:
             emojiChoices = faceEmojis
             gameBoard.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
             cardBackColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
             cardFrontColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+            cardBorderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         }
     }
     
@@ -100,11 +107,20 @@ class ViewController: UIViewController {
                 button.backgroundColor = cardFrontColor
             } else {
                 button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? UIColor.clear : cardBackColor
+                button.backgroundColor = cardBackColor
             }
             // rounded corners
             button.layer.cornerRadius = 10
             button.clipsToBounds = true
+            // boarders
+            button.layer.borderColor = cardBorderColor.cgColor
+            button.layer.borderWidth = 2
+            // Hide matched cards
+            if card.isMatched {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = UIColor.clear
+                button.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+            }
         }
     }
     
